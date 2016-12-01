fbecda0bc3683736aa8016f8088ecd82cd6a567e
Super_Frelon <truc5200@gmail.com>
2016-12-01 12:28:55 +0100

    Fix some bugs

--- ZooRunner/ZooRunner.GUI/MainForm.Designer.cs
+++ ZooRunner/ZooRunner.GUI/MainForm.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this._controlPanel = new ZooRunner.GUI.ControlPanel();
-            this._zooViewPort = new ZooRunner.ZooViewPortControl();
             this._viewPortDemoControl = new ZooRunner.GUI.ViewPortDemoControl();
             this.SuspendLayout();
             // 
@@ -43,18 +42,6 @@
             this._controlPanel.Size = new System.Drawing.Size(1216, 101);
             this._controlPanel.TabIndex = 4;
             // 
-            // _zooViewPort
-            // 
-            this._zooViewPort.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this._zooViewPort.BoxCount = 20;
-            this._zooViewPort.Location = new System.Drawing.Point(352, 12);
-            this._zooViewPort.Name = "_zooViewPort";
-            this._zooViewPort.Size = new System.Drawing.Size(876, 423);
-            this._zooViewPort.TabIndex = 3;
-            this._zooViewPort.Text = "_zooViewPortControl";
-            // 
             // _viewPortDemoControl
             // 
             this._viewPortDemoControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -65,17 +52,15 @@
             this._viewPortDemoControl.Margin = new System.Windows.Forms.Padding(5);
             this._viewPortDemoControl.Name = "_viewPortDemoControl";
             this._viewPortDemoControl.ShowGridLines = true;
-            this._viewPortDemoControl.Size = new System.Drawing.Size(330, 421);
+            this._viewPortDemoControl.Size = new System.Drawing.Size(1216, 421);
             this._viewPortDemoControl.TabIndex = 0;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1240, 556);
             this.Controls.Add(this._controlPanel);
-            this.Controls.Add(this._zooViewPort);
             this.Controls.Add(this._viewPortDemoControl);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -88,7 +73,6 @@
         #endregion
         private ViewPortDemoControl _viewPortDemoControl;
         private ControlPanel _controlPanel;
-        private ZooViewPortControl _zooViewPort;
     }
 }
 
